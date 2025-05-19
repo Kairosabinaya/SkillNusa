3a76d4cd89ee588383d75213534464540fb3b2ae
Kairos Abinaya Susanto <kairos.susanto@binus.ac.id>
2025-05-19 18:26:32 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: {
+            DEFAULT: '#010042',
+            light: '#1a1a5a',
+            dark: '#00002e',
+          }
+        },
+      },
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
+  }
